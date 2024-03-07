673d3b25f36771f21ca0535adab016fe125e5a0c
AndrMaulana <andryymaulana2000@gmail.com>
2024-03-07 20:47:59 +0700

    edit web service

--- A. Pemrograman Golang Dasar/44 Web Service API Server/web_service.go
+++ A. Pemrograman Golang Dasar/44 Web Service API Server/web_service.go
@@ -39,7 +39,7 @@ func users(w http.ResponseWriter, r *http.Request) {
 func user(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	if r.Method == "GET" {
+	if r.Method == "POST" {
 		id := r.FormValue("id")
 		var result []byte
 		var err error
