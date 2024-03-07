548d1c6a9af8c22f56f6a6e1adf6f70e9252b190
AndrMaulana <andryymaulana2000@gmail.com>
2024-03-07 20:48:12 +0700

    add simple http request

--- A. Pemrograman Golang Dasar/45 Http Request/http_request.go
+++ A. Pemrograman Golang Dasar/45 Http Request/http_request.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 var baseUrl = "http://localhost:8080"
@@ -13,7 +17,7 @@ type Student struct {
 	Grade int
 }
 
-func fecthUser() ([]Student, error) {
+func fetchUsers() ([]Student, error) {
 	var err error
 	var client = &http.Client{}
 	var data []Student
@@ -22,16 +26,65 @@ func fecthUser() ([]Student, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	response, err := client.Do(request)
 	if err != nil {
 		return nil, err
 	}
-
 	defer response.Body.Close()
-	err = json.NewDecoder(response.Body).Decode(&data)
 
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 
-func main() {
+func fetchUser(ID string) (Student, error) {
+	var err error
+	var client = &http.Client{}
+	var data Student
 
+	var param = url.Values{}
+	param.Set("id", ID)
+	var payload = bytes.NewBufferString(param.Encode())
+
+	request, err := http.NewRequest("POST", baseUrl+"/user", payload)
+	if err != nil {
+		return data, err
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response, err := client.Do(request)
+	if err != nil {
+		return data, err
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+func main() {
+	var users, err = fetchUsers()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	for _, each := range users {
+		fmt.Printf("ID: %s\t Name: %s\t Grade : %d\n", each.ID, each.Name, each.Grade)
+	}
+
+	fmt.Println(strings.Repeat("*", 50))
+	var user1, _ = fetchUser("S002")
+	if err != nil {
+		fmt.Println("Error!", err.Error())
+		return
+	}
+
+	fmt.Printf("ID: %s\t Name: %s\t Grade: %d\n", user1.ID, user1.Name, user1.Grade)
 }
